27db33e8ed28a25c849bdd32a93c68b64b25d0f4
Christian <leec22@rpi.edu>
2018-10-07 13:29:14 -0400

    Added Cone Check

--- Assets/Scripts/AI/ConeCheck.cs
+++ Assets/Scripts/AI/ConeCheck.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+// Create a cone to detect collisions
+[RequireComponent(typeof(Separate))]
+public class ConeCheck : AI {
+    // Avoid group
+    [HideInInspector]
+    public List<NPCController> targets;
+    [SerializeField]
+    private float threshold;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Define Output
+    override public Steering Output(NPCController lead) {
+        // Define variables
+        Vector2 orientation = new Vector2(Mathf.Sin(player.rb.rotation), Mathf.Cos(player.rb.rotation));
+        Steering result = new Steering();
+        int count = 0;
+
+        // Obtain the averaged data for nearby boids and calculate separation velocity
+        foreach (NPCController target in targets) {
+            if (Vector2.Dot(orientation, (target.rb.position - player.rb.position)) < threshold) {
+                result += GetComponent<Separate>().Output(target);
+                count++;
+            }
+        }
+        return result /= Mathf.Max(count, 1);
+    }
 }
